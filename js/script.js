8b3af9082cb64a42e9528c72d01616e99bc6c57f
SebL68 <sebastien.lehmann0@gmail.com>
2019-09-16 15:21:04 +0200

    Responsive suite

--- js/script.js
+++ js/script.js
@@ -219,18 +219,20 @@ window.addEventListener("load", function () {
 
 	var paraEl = document.querySelectorAll("[data-speed]");
 	function parallax() {
-		paraEl.forEach(e => {
-			var rect = e.getBoundingClientRect();
-			var transY = e.dataset.offset || 0;
-			var posiOrigin = rect.top - transY + rect.height / 2;
-			var offset = (posiOrigin - window.innerHeight / 2) * e.dataset.speed;
+		if(window.matchMedia("(min-width: 1000px)").matches){
+			paraEl.forEach(e => {
+				var rect = e.getBoundingClientRect();
+				var transY = e.dataset.offset || 0;
+				var posiOrigin = rect.top - transY + rect.height / 2;
+				var offset = (posiOrigin - window.innerHeight / 2) * e.dataset.speed;
 
-			if (rect.top < window.innerHeight ||
-				offset < window.innerHeight / 2) {
-				e.dataset.offset = offset;
-				e.style.transform = `translateY(${offset}px)`;
-			}
-		})
+				if (rect.top < window.innerHeight ||
+					offset < window.innerHeight / 2) {
+					e.dataset.offset = offset;
+					e.style.transform = `translateY(${offset}px)`;
+				}
+			})
+		}
 	}
 	window.addEventListener("scroll", parallax, { passive: true });
 	parallax();
