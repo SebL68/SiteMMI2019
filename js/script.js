90fe6e2a459cef90de8083bdb477d0b8117127a7
SebL68 <sebastien.lehmann0@gmail.com>
2019-09-18 15:18:33 +0200

    Retirer les class hors viewport

--- js/script.js
+++ js/script.js
@@ -54,6 +54,7 @@ function prepare() {
 	/*  
 Relier par une courbe ou une ligne deux éléments HTML.
 Gère le redimentionnement de pages.
+Gère les changements de styles appliqués aux objets par la suite.
 PathLength à 100.
 
 HTML : 
@@ -223,16 +224,24 @@ new CurveSVG(
 		if (window.matchMedia("(min-width: 1000px)").matches) {
 			document.querySelectorAll("[data-class]").forEach(e => {
 				if (e.getBoundingClientRect().y < window.innerHeight - (e.dataset.offset || 0)) {
-					setTimeout(() => {
-						e.classList.add(e.dataset.class);
-						e.removeAttribute("data-class");
-					}, e.dataset.delay || 0)
+					if(!e.classList.contains(e.dataset.class) && (e.dataset.timer || "") == ""){
+						e.dataset.timer = setTimeout(() => {
+							e.classList.add(e.dataset.class);
+							clearTimeout(e.dataset.timer);
+							e.dataset.timer = "";
+							//e.removeAttribute("data-class");
+						}, e.dataset.delay || 0)
+					}
+				} else {
+					if(e.dataset.timer)clearTimeout(e.dataset.timer);
+					e.classList.remove(e.dataset.class);
 				}
 			})
 		}
 	}
 	window.addEventListener("scroll", addClass, { passive: true });
 	addClass();
+	
 
 	/****************/
 	/* Paralaxe */
